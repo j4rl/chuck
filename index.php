72eab28a5edb3574a8505fb99b35378cc4f40419
j4rl <charlie.jarl@gmail.com>
2026-03-25 14:24:29 +0100

    0.0.4

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
 declare(strict_types=1);
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Content-Type: application/json; charset=utf-8');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
+
 header('Content-Type: application/json; charset=utf-8');
 
 /*
